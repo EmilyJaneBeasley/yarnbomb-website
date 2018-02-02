955def8bf5b587e62b51aa703deebd8a8f31a721
EmilyJaneBeasley <mizem1977@yahoo.com>
2018-02-01 18:01:31 -0600

    Initial commit

---
+++ src/main/java/com/example/yarnbombwebsite/YarnbombWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.yarnbombwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarnbombWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YarnbombWebsiteApplication.class, args);
+	}
+}
+
---
+++ src/test/java/com/example/yarnbombwebsite/YarnbombWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.yarnbombwebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YarnbombWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
