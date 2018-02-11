2e061f7939d4fd4c0c6f85f450ea40c93f113378
EmilyJaneBeasley <mizem1977@yahoo.com>
2018-02-11 17:01:42 -0600

    Worked on biography page

--- src/main/java/com/example/yarnbombwebsite/controllers/HomepageController.java
+++ src/main/java/com/example/yarnbombwebsite/controllers/HomepageController.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("views")
 
 public class HomepageController {
-    @RequestMapping(value = "")
 
+    @RequestMapping(value = "")
     public String index (Model model){
         model.addAttribute("title", "RiverBend YarnBombers");
         model.addAttribute("intro","We help make yarn art happen in the RiverBend area");
+        model.addAttribute("signup", "Don't have an account? Create one here to contribute " +
+                "to our blog and get email updates on upcoming events.");
         return "views/index";
+    }
+    @RequestMapping(value = "biography")
+    public String displayBiographyPage(Model model) {
+        model.addAttribute("title", "About Our Members");
+        return "views/biography";
     }
 
 
