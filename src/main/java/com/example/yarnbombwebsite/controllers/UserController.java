0e202c6219b79f95022e039da192861ff5e32ab8
EmilyJaneBeasley <mizem1977@yahoo.com>
2018-02-26 18:40:12 -0600

    Added user sign up

---
+++ src/main/java/com/example/yarnbombwebsite/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.example.yarnbombwebsite.controllers;
+
+
+import com.example.yarnbombwebsite.models.User;
+import com.example.yarnbombwebsite.models.UserData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        model.addAttribute(new User());
+        return "/user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute(user);
+            return "user/add";
+        }
+
+        if (user.getPassword() == null || verify == null) {
+            user.setPassword("");
+            model.addAttribute("passwordError", "Enter your password and verify it.");
+            model.addAttribute(user);
+            return "user/add";
+        }
+
+        if (!user.getPassword().equals(verify)) {
+            user.setPassword("");
+            model.addAttribute("passwordError", "Passwords don't match.");
+            model.addAttribute(user);
+            return "user/add";
+        }
+        else {
+            UserData.add(user);
+            model.addAttribute(user);
+            return "user/index";
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/yarnbombwebsite/models/User.java
@@ -0,0 +1,73 @@
+package com.example.yarnbombwebsite.models;
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class User {
+
+    @NotNull
+    @Size(min=5, max=15, message = "Username must be between 5 and 15 characters.")
+    private String username;
+
+    @Email(message = "Please enter a valid email")
+    private String email;
+
+    @NotNull
+    @Size(min=6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private int userId;
+    private Date joined;
+    private static int nextUserId = 1;
+
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userId = nextUserId;
+        this.joined = new Date();
+        nextUserId++;
+    }
+
+    public User() {
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public Date getJoinedDate() {
+        return joined;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
